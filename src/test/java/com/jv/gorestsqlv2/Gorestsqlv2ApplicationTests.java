9ba24f446eea0d9bd7d2a4392d4f03419115606b
jailsonveiga <jmdaveiga2012@gmail.com>
2022-05-07 19:25:10 -0400

    GorestSQL Version 2 Gorestsqlv2ApplicationTests.java

---
+++ src/test/java/com/jv/gorestsqlv2/Gorestsqlv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jv.gorestsqlv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gorestsqlv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
