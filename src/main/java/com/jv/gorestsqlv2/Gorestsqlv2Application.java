2fdaf12a932a09258da3eab4538bd3501b4490e2
jailsonveiga <jmdaveiga2012@gmail.com>
2022-05-07 19:24:49 -0400

    GorestSQL Version 2 Gorestsqlv2Application.java

---
+++ src/main/java/com/jv/gorestsqlv2/Gorestsqlv2Application.java
@@ -0,0 +1,13 @@
+package com.jv.gorestsqlv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gorestsqlv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gorestsqlv2Application.class, args);
+	}
+
+}
