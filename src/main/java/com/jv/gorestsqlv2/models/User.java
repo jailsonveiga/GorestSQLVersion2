0be87de7b953f9ef0d536da451af0f78e87f7b93
jailsonveiga <jmdaveiga2012@gmail.com>
2022-05-07 21:25:11 -0400

    GorestSQL Version 2 User.java

---
+++ src/main/java/com/jv/gorestsqlv2/models/User.java
@@ -0,0 +1,60 @@
+package com.jv.gorestsqlv2.models;
+
+/*
+   {
+        "id": 4033,
+        "name": "Minakshi Bharadwaj",
+        "email": "minakshi_bharadwaj@kuhic.org",
+        "gender": "female",
+        "status": "inactive"
+    },
+*/
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
+
+    private String email;
+
+    private String gender;
+
+    @Column ()
+    private String status;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", gender='" + gender + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
+}
