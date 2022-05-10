006d805e89e7e244d7aab555733cc476f634fd23
jailsonveiga <jmdaveiga2012@gmail.com>
2022-05-10 16:30:33 -0400

    GorestSQL Version 2 ApiErrorHandling.java

--- src/main/java/com/jv/gorestsqlv2/utils/ApiErrorHandling.java
+++ src/main/java/com/jv/gorestsqlv2/utils/ApiErrorHandling.java
@@ -1,5 +1,17 @@
 package com.jv.gorestsqlv2.utils;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 public class ApiErrorHandling {
+    public static ResponseEntity<?> genericApiError(Exception e) {
+        System.out.println(e.getMessage());
+        System.out.println(e.getClass());
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    public static ResponseEntity<?> customApiError(String message, HttpStatus status) {
+        return new ResponseEntity<>(message, status);
+    }
 
 }
