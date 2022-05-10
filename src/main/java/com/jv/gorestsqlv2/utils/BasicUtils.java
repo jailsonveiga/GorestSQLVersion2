ebbf95ea15c3e01fc1978e3c379c73456256fce7
jailsonveiga <jmdaveiga2012@gmail.com>
2022-05-10 16:30:49 -0400

    GorestSQL Version 2 BasicUtils.java

--- src/main/java/com/jv/gorestsqlv2/utils/BasicUtils.java
+++ src/main/java/com/jv/gorestsqlv2/utils/BasicUtils.java
@@ -1,4 +1,15 @@
 package com.jv.gorestsqlv2.utils;
 
 public class BasicUtils {
+    public static boolean isStrNaN(String strNum) {
+        if (strNum == null) {
+            return true;
+        }
+        try {
+            Integer.parseInt(strNum);
+        } catch(NumberFormatException e) {
+            return true;
+        }
+        return false;
+    }
 }
