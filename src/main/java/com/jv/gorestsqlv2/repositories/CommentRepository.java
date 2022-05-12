091c3ed9da1701ea810545b8c26029a176246b64
jailsonveiga <jmdaveiga2012@gmail.com>
2022-05-12 17:41:21 -0400

    GorestSQL Version 2 CommentRepository.java

--- src/main/java/com/jv/gorestsqlv2/repositories/CommentRepository.java
+++ src/main/java/com/jv/gorestsqlv2/repositories/CommentRepository.java
@@ -1,4 +1,7 @@
 package com.jv.gorestsqlv2.repositories;
 
-public interface CommentRepository {
+import com.jv.gorestsqlv2.models.Comment;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CommentRepository extends CrudRepository<Comment, Long> {
 }
