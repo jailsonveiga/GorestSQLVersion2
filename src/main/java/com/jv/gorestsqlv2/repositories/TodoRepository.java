3962c00f78919cf736ca828b0a6746814f8c5fc3
jailsonveiga <jmdaveiga2012@gmail.com>
2022-05-12 17:41:49 -0400

    GorestSQL Version 2 TodoRepository.java

--- src/main/java/com/jv/gorestsqlv2/repositories/TodoRepository.java
+++ src/main/java/com/jv/gorestsqlv2/repositories/TodoRepository.java
@@ -1,4 +1,7 @@
 package com.jv.gorestsqlv2.repositories;
 
-public class TodoRepository {
+import com.jv.gorestsqlv2.models.Todo;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TodoRepository extends CrudRepository<Todo, Long> {
 }
