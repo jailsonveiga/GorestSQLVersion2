2948c47cc32b1674aabbfb1f3e7afe0528e58737
jailsonveiga <jmdaveiga2012@gmail.com>
2022-05-17 17:45:45 -0400

    GorestSQL Version 2 Comment.java

--- src/main/java/com/jv/gorestsqlv2/models/Comment.java
+++ src/main/java/com/jv/gorestsqlv2/models/Comment.java
@@ -1,4 +1,57 @@
 package com.jv.gorestsqlv2.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private long post_id;
+
+    private String name;
+
+    private String email;
+
+    private String body;
+
+    public long getId() {
+        return id;
+    }
+
+    public long getPost_id() {
+        return post_id;
+    }
+
+    public void setPost_id(long post_id) {
+        this.post_id = post_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id=" + id +
+                ", post_id=" + post_id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", body='" + body + '\'' +
+                '}';
+    }
 }
