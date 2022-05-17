35436e8c69795faa381c9d48fb580901cc3544ae
jailsonveiga <jmdaveiga2012@gmail.com>
2022-05-17 17:47:04 -0400

    GorestSQL Version 2 Post.java

--- src/main/java/com/jv/gorestsqlv2/models/Post.java
+++ src/main/java/com/jv/gorestsqlv2/models/Post.java
@@ -1,4 +1,53 @@
 package com.jv.gorestsqlv2.models;
 
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Entity
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private long user_id;
+
+    private String title;
+
+    @Column(length = 512) // VARCHAR(255)
+    private String body;
+
+    public long getId() {
+        return id;
+    }
+
+    public long getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(long user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", user_id=" + user_id +
+                ", title='" + title + '\'' +
+                ", body='" + body + '\'' +
+                '}';
+    }
 }
