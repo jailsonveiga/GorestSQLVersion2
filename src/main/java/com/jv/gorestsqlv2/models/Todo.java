6b7e38b23d16d8c664d8f7629dbd6446f3e8d95b
jailsonveiga <jmdaveiga2012@gmail.com>
2022-05-20 17:42:42 -0400

    GorestSQL Version 2 Todo.java

--- src/main/java/com/jv/gorestsqlv2/models/Todo.java
+++ src/main/java/com/jv/gorestsqlv2/models/Todo.java
@@ -1,4 +1,58 @@
 package com.jv.gorestsqlv2.models;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private long user_id;
+
+    private String title;
+
+    private String due_on;
+
+    private String status;
+
+    public long getId() {
+        return id;
+    }
+
+    public long getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(long user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDue_on() {
+        return due_on;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    @Override
+    public String toString() {
+        return "Todo{" +
+                "id=" + id +
+                ", user_id=" + user_id +
+                ", title='" + title + '\'' +
+                ", due_on='" + due_on + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
 }
